d937540508283408df13467a588eb7a401ca78a0
Mirosław Szajner <mszajner@rexoft.pl>
2014-05-07 09:24:38 +0200

    First import

---
+++ README.md
@@ -0,0 +1,10 @@
+jQuery Autofilter Plugin
+========================
+
+This is cross-browser plugin to filter text typed in form fields.
+
+@TODO
+-----
+
+* Documentation
+* Tests
---
+++ jquery.autofilter.plugin.js
@@ -0,0 +1,44 @@
+(function($) {
+	
+	$.fn.autofilter = function (filterCallback) {
+		if (!filterCallback) { 
+			filterCallback = function() { 
+				return $(this).val(); 
+			};
+		}
+		return this.each(function() {
+			$(this).keyup(function(ev){
+				var currentValue = filterCallback.apply(this);
+				$(this).data('currentValue', currentValue);
+				if (typeof this.innerText == 'undefined') {
+					$(this).val(currentValue);
+				} else {
+					var selectionStart = this.selectionStart;
+					var selectionEnd = this.selectionEnd;
+					try {
+						this.innerText = val;
+					} catch (e) {
+						this.value = val;
+					}
+					this.selectionStart = selectionStart;
+					this.selectionEnd = selectionEnd;
+				}
+			}).focusin(function() {
+				if ($.browser.safari) {
+					var previousValue = $(this).val();
+					$(this).data('previousValue', previousValue);
+					$(this).data('currentValue', previousValue);
+				}
+			}).focusout(function() {
+				if ($.browser.safari) {
+					var previousValue = $(this).data('previousValue');
+					var currentValue = $(this).data('currentValue');
+					if (previousValue != currentValue) {
+						$(this).trigger('change');
+					}
+				}
+			});
+		}
+	};
+	
+})(jQuery);
